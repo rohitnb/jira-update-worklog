eba54c011dbf754a1b2cfe22758cc1129af340a4
rohitnb <rohitnb@github.com>
2021-05-17 13:04:31 +0530

    Remove label from the action

--- dist/index.js
+++ dist/index.js
@@ -8702,7 +8702,10 @@ const jiraIssue = core.getInput('jira-issue');
 const jiraToken = core.getInput('jira-token'); 
 const jiraDomain = core.getInput('jira-domain');
 const prLabel = core.getInput('prLabel');
+const ghtoken = core.getInput('ghtoken');
 //END INPUTS
+
+const octokit = github.getOctokit(ghtoken);
 
 async function run(){
     const label_wl = prLabel.split(":")[1];
@@ -8732,6 +8735,13 @@ async function run(){
 
     updateWorklog(function(result){
         console.log(label_wl+" was added to the worklog for "+jiraIssue);
+        console.log("Removing the "+prLabel+" label from this PR now");
+        var removedLabel = octokit.rest.issues.removeLabel({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: github.context.payload.pull_request.number,
+            name: prLabel
+          });
         core.setOutput("result",true);
     });
 }
--- src/jira-update-worklog.js
+++ src/jira-update-worklog.js
@@ -8,7 +8,10 @@ const jiraIssue = core.getInput('jira-issue');
 const jiraToken = core.getInput('jira-token'); 
 const jiraDomain = core.getInput('jira-domain');
 const prLabel = core.getInput('prLabel');
+const ghtoken = core.getInput('ghtoken');
 //END INPUTS
+
+const octokit = github.getOctokit(ghtoken);
 
 async function run(){
     const label_wl = prLabel.split(":")[1];
@@ -38,6 +41,13 @@ async function run(){
 
     updateWorklog(function(result){
         console.log(label_wl+" was added to the worklog for "+jiraIssue);
+        console.log("Removing the "+prLabel+" label from this PR now");
+        var removedLabel = octokit.rest.issues.removeLabel({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: github.context.payload.pull_request.number,
+            name: prLabel
+          });
         core.setOutput("result",true);
     });
 }
